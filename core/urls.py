7eda0415b4711570c804742445c0e7fb71fb5d29
Max Kaening <maximilian.kaening@gmail.com>
2025-01-18 16:52:45 +0000

    Add check-availability url

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('campervans/', views.campervan_list, name='campervan_list'),
+    path('check-availability/', views.check_availability, name='check_availability'),
 ]
