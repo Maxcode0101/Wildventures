46755d13699f723629d19a7a96409a9713f8d230
Max Kaening <maximilian.kaening@gmail.com>
2025-01-20 09:44:49 +0000

    Create booking form

---
+++ booking/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Booking
+
+class BookingForm(forms.ModelForm):
+    """Form for campervan bookings."""
+    class Meta:
+        model = Booking
+        fields = ['start_date', 'end_date']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['start_date'].widget.attrs.update({'class': 'datepicker'})
+        self.fields['end_date'].widget.attrs.update({'class': 'datepicker'})
