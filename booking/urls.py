e3b5af596161b6e38619ba16e2ad6c74fe5cf052
Max Kaening <maximilian.kaening@gmail.com>
2025-01-21 12:02:31 +0000

    Define url for my bookings page

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('book/<int:campervan_id>/', views.book_campervan, name='book_campervan'),
     path('booking-confirmation/<int:booking_id>/', views.booking_confirmation, name='booking_confirmation'),
+    path('my_bookings/', views.my_bookings, name='my_bookings'),
 ]
