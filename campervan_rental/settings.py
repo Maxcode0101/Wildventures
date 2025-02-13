04af0845119d799dc312675eab5cb6a4d1a5e669
MaxCode0101 <maximilian.kaening@gmail.com>
2025-02-13 11:05:47 +0100

    Add pay now button to my bookings page and view booking details page

--- campervan_rental/settings.py
+++ campervan_rental/settings.py
@@ -10,11 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import ssl
+import certifi
 from pathlib import Path
 import os
 import dj_database_url
 if os.path.exists("env.py"):
     import env
+    
+ssl._create_default_https_context = ssl.create_default_context(cafile=certifi.where())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
